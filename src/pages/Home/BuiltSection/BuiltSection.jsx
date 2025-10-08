44231684533a43a3e4f149506fa4791bba56933a
Shahariya-17 <shahariyarrakib780@gmail.com>
2025-10-08 22:22:12 +0600

    added built section

---
+++ src/pages/Home/BuiltSection/BuiltSection.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const BuiltSection = () => {
+    return (
+        <div className='bg-lime-400 text-black'>
+           <div className='w-8/12 mx-auto py-20'>
+             <h1 className='text-8xl font-semibold'>Built for AI agents and more</h1>
+             <div className='grid grid-cols-1 md:grid-cols-2'>
+                <div>
+                    
+                </div>
+             </div>
+           </div>
+        </div>
+    );
+};
+
+export default BuiltSection;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import MarqueeSection from '../Marquee/MarqueeSection';
 import SecureCard from '../SecureCard/SecureCard';
+import BuiltSection from '../BuiltSection/BuiltSection';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner></Banner>
             <MarqueeSection></MarqueeSection>
             <SecureCard></SecureCard>
+            <BuiltSection></BuiltSection>
         </div>
     );
 };
